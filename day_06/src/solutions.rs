219e57b4af815dbd22ea16ba054b45094a3f3e6b
Matt Puentes <mpuentes@mitre.org>
2023-12-06 09:43:08 -0500

    Day 06

--- day_06/src/solutions.rs
+++ day_06/src/solutions.rs
@@ -1,7 +1,43 @@
 pub fn pt_1(str_input: &str) {
-    println!("Part 1 result: {}", str_input.len())
+    let (time, distance) = str_input.split_once('\n').unwrap();
+    let times = time[9..]
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<f32>().unwrap())
+        .collect::<Vec<_>>();
+    let distances = distance[9..]
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<f32>().unwrap())
+        .collect::<Vec<_>>();
+
+    let mut product = 1.0;
+    for (time, record_distance) in times.into_iter().zip(distances.into_iter()) {
+        let solve1 = -(-time + ((time.powi(2) + 4.0 * (-1.0 - record_distance)).sqrt())) / 2.0;
+        let solve2 = -(-time - ((time.powi(2) + 4.0 * (-1.0 - record_distance)).sqrt())) / 2.0;
+        println!("Solve 1: {} Solve 2: {}", solve1.ceil(), solve2.floor());
+        println!("num: {}", ((solve2.floor() - solve1.ceil()) + 1.0));
+        product *= (solve2.floor() - solve1.ceil()) + 1.0;
+    }
+
+    println!("Part 1 result: {}", product)
 }
 
 pub fn pt_2(str_input: &str) {
-    println!("Part 2 result: {}", str_input.len())
+    let (time_str, distance_str) = str_input.split_once('\n').unwrap();
+    let time: f64 = time_str
+        .chars()
+        .filter(|s| s.is_numeric())
+        .collect::<String>()
+        .parse()
+        .unwrap();
+    let distance: f64 = distance_str
+        .chars()
+        .filter(|s| s.is_numeric())
+        .collect::<String>()
+        .parse()
+        .unwrap();
+
+    let solve1 = -(-time + ((time.powi(2) + 4.0 * (-1.0 - distance)).sqrt())) / 2.0;
+    let solve2 = -(-time - ((time.powi(2) + 4.0 * (-1.0 - distance)).sqrt())) / 2.0;
+
+    println!("Part 2 result: {}", (solve2.floor() - solve1.ceil()) + 1.0)
 }
