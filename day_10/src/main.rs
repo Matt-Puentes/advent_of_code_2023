5ea909512b68b461959e2a2a7e15ccf6b1793842
Matt Puentes <mpuentes@mitre.org>
2023-12-11 15:28:46 -0500

    Day 10

--- day_10/src/main.rs
+++ day_10/src/main.rs
@@ -13,6 +13,12 @@ pub fn parse_args() -> Test<'static> {
     let text = if args.len() > 2 {
         if args[2].eq("example") {
             include_str!("../example.txt")
+        } else if args[2].eq("example2") {
+            include_str!("../example2.txt")
+        } else if args[2].eq("example3") {
+            include_str!("../example3.txt")
+        } else if args[2].eq("example4") {
+            include_str!("../example4.txt")
         } else {
             panic!("second argument can only be 'example'")
         }
--- day_10/src/solutions.rs
+++ day_10/src/solutions.rs
@@ -1,96 +1,16 @@
-// .....
-// .S-7.
-// .|.|.
-// .L-J.
-// .....
-
-// | is a vertical pipe connecting north and south.
-// - is a horizontal pipe connecting east and west.
-// L is a 90-degree bend connecting north and east.
-// J is a 90-degree bend connecting north and west.
-// 7 is a 90-degree bend connecting south and west.
-// F is a 90-degree bend connecting south and east.
-// . is ground; there is no pipe in this tile.
-// S is the starting position of the animal; there is a pipe on this tile, but your sketch doesn't show what shape the pipe has.
-
-// use std::fmt::{Debug, Display};
-
-// enum Pipe {
-//     Up,
-//     Side,
-//     NE,
-//     NW,
-//     SW,
-//     SE,
-//     Ground,
-//     Start,
-// }
-
-// impl Display for Pipe {
-//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-//         match self {
-//             Pipe::Up => write!(f, "|"),
-//             Pipe::Side => write!(f, "-"),
-//             Pipe::NE => write!(f, "└"),
-//             Pipe::NW => write!(f, "┘"),
-//             Pipe::SW => write!(f, "┐"),
-//             Pipe::SE => write!(f, "┌"),
-//             Pipe::Ground => write!(f, "."),
-//             Pipe::Start => write!(f, "s"),
-//         }
-//     }
-// }
-
-// impl From<char> for Pipe {
-//     fn from(value: char) -> Self {
-//         match value {
-//             '|' => Pipe::Up,
-//             '-' => Pipe::Side,
-//             'L' => Pipe::NE,
-//             'J' => Pipe::NW,
-//             '7' => Pipe::SW,
-//             'F' => Pipe::SE,
-//             '.' => Pipe::Ground,
-//             'S' => Pipe::Start,
-//             c => panic!("Unknown character '{}'", c),
-//         }
-//     }
-// }
-
-// let map_string = map
-//     .iter()
-//     .enumerate()
-//     .map(|(l, s)| {
-//         s.iter()
-//             .enumerate()
-//             .map(|(i, c)| if vm[l][i] { c } else { &'.' })
-//             .collect::<String>()
-//     })
-//     .collect::<Vec<_>>()
-//     .join("\n");
-// println!("Map:\n{map_string}");
-
-static UP: char = '|';
-static SIDE: char = '-';
-static NE: char = 'L';
-static NW: char = 'J';
-static SW: char = '7';
-static SE: char = 'F';
-static START: char = 'S';
-// static GROUND: char = '.';
-static PDOWN: [char; 4] = [UP, SW, SE, START];
-static PUP: [char; 4] = [UP, NW, NE, START];
-static PLEFT: [char; 4] = [SIDE, NW, SW, START];
-static PRIGHT: [char; 4] = [SIDE, NE, SE, START];
+static PD: [char; 4] = ['|', '7', 'F', 'S'];
+static PU: [char; 4] = ['|', 'J', 'L', 'S'];
+static PL: [char; 4] = ['-', 'J', '7', 'S'];
+static PR: [char; 4] = ['-', 'L', 'F', 'S'];
 
 pub fn pt_1(str_input: &str) {
     let mut start_idx: (usize, usize) = (0, 0);
-    // Collect the input into a 2d matrix of chars, also search for 'S'
     let map: Vec<Vec<char>> = str_input.lines().map(|l| l.chars().collect()).collect();
-    for (l, line) in map.iter().enumerate() {
+    'out: for (l, line) in map.iter().enumerate() {
         for (c, char) in line.iter().enumerate() {
             if *char == 'S' {
-                start_idx = (l, c)
+                start_idx = (l, c);
+                break 'out;
             }
         }
     }
@@ -104,22 +24,21 @@ pub fn pt_1(str_input: &str) {
 
     let mut queue: Vec<((usize, usize), usize)> = vec![(start_idx, 0)];
     let mut highest: usize = 0;
-    loop {
-        let Some(((l, c), d)) = queue.pop() else { break };
+    while let Some(((l, c), d)) = queue.pop() {
         let char = &map[l][c];
-        vm[l][c] = true;
         highest = highest.max(d);
+        vm[l][c] = true;
 
-        if PUP.contains(char) && l > 0 && PDOWN.contains(&map[l - 1][c]) && !vm[l - 1][c] {
+        if PU.contains(char) && l > 0 && PD.contains(&map[l - 1][c]) && !vm[l - 1][c] {
             queue.push(((l - 1, c), d + 1))
         }
-        if PDOWN.contains(char) && l < h - 1 && PUP.contains(&map[l + 1][c]) && !vm[l + 1][c] {
+        if PD.contains(char) && l < h - 1 && PU.contains(&map[l + 1][c]) && !vm[l + 1][c] {
             queue.push(((l + 1, c), d + 1))
         }
-        if PLEFT.contains(char) && c > 0 && PRIGHT.contains(&map[l][c - 1]) && !vm[l][c - 1] {
+        if PL.contains(char) && c > 0 && PR.contains(&map[l][c - 1]) && !vm[l][c - 1] {
             queue.push(((l, c - 1), d + 1))
         }
-        if PRIGHT.contains(char) && c < w - 1 && PLEFT.contains(&map[l][c + 1]) && !vm[l][c + 1] {
+        if PR.contains(char) && c < w - 1 && PL.contains(&map[l][c + 1]) && !vm[l][c + 1] {
             queue.push(((l, c + 1), d + 1))
         }
     }
@@ -128,89 +47,65 @@ pub fn pt_1(str_input: &str) {
 }
 
 pub fn pt_2(str_input: &str) {
-    let mut start_idx: (usize, usize) = (0, 0);
-    // Collect the input into a 2d matrix of chars, also search for 'S'
-    let map: Vec<Vec<char>> = str_input
-        .lines()
-        .enumerate()
-        .map(|(l, s)| {
-            s.chars()
-                .enumerate()
-                .map(|(i, c)| {
-                    if c == 'S' {
-                        start_idx = (l, i)
-                    };
-                    c
-                })
-                .collect()
-        })
-        .collect();
+    let mut start: (usize, usize) = (0, 0);
+    // Collect the input into a 2d matrix of chars, also search for ground
+    let map: Vec<Vec<char>> = str_input.lines().map(|l| l.chars().collect()).collect();
+    'out: for (l, line) in map.iter().enumerate() {
+        for (c, char) in line.iter().enumerate() {
+            if *char == 'S' {
+                start = (l, c);
+                break 'out;
+            }
+        }
+    }
 
-    // Height
     let h = map.len();
-    // Width
     let w = map[0].len();
     // Visited map
-    // let mut vm = vec![vec![false; w]; h];
+    let mut vm = vec![vec!['.'; w]; h];
 
-    let mut queue: Vec<Vec<(usize, usize)>> = vec![vec![start_idx]];
-    let loop_idxs: Vec<(usize, usize)> = loop {
-        let vec = queue.pop().expect("Queue should not be empty"); // else { break };
-        let (l, c) = vec.last().unwrap();
-        let ch = &map[*l][*c];
-        // if vec.len() > 1 && *ch == START {
-        //     // println!("{vec:?}");
-        //     // panic!("Found loop!");
-        // };
+    let mut queue: Vec<(usize, usize)> = vec![start];
+    while let Some((l, c)) = queue.pop() {
+        let ch = &map[l][c];
+        vm[l][c] = *ch;
 
-        // println!("{vec:?}");
-        if PUP.contains(ch) && *l > 0 && PDOWN.contains(&map[*l - 1][*c]) {
-            if vec.len() > 2 && map[*l - 1][*c] == START {
-                // loop found
-                break vec;
-            }
-            if !vec.contains(&(l - 1, *c)) {
-                let mut newvec = vec.clone();
-                newvec.push((*l - 1, *c));
-                queue.push(newvec)
-            }
+        // Check if the neighbor is in bounds, valid, and not already processed.
+        if l > 0 && PU.contains(ch) && PD.contains(&map[l - 1][c]) && vm[l - 1][c] == '.' {
+            queue.push((l - 1, c))
         }
-        if PDOWN.contains(ch) && *l < h - 1 && PUP.contains(&map[*l + 1][*c]) {
-            if vec.len() > 2 && map[*l + 1][*c] == START {
-                // loop found
-                break vec;
-            }
-            if !vec.contains(&(l + 1, *c)) {
-                let mut newvec = vec.clone();
-                newvec.push((*l + 1, *c));
-                queue.push(newvec)
-            }
+        if l < h - 1 && PD.contains(ch) && PU.contains(&map[l + 1][c]) && vm[l + 1][c] == '.' {
+            queue.push((l + 1, c))
         }
-        if PLEFT.contains(ch) && *c > 0 && PRIGHT.contains(&map[*l][*c - 1]) {
-            if vec.len() > 2 && map[*l][*c - 1] == START {
-                // loop found
-                break vec;
-            }
-            if !vec.contains(&(*l, c - 1)) {
-                let mut newvec = vec.clone();
-                newvec.push((*l, *c - 1));
-                queue.push(newvec)
-            }
+        if c > 0 && PL.contains(ch) && PR.contains(&map[l][c - 1]) && vm[l][c - 1] == '.' {
+            queue.push((l, c - 1))
         }
-        if PRIGHT.contains(ch) && *c < w - 1 && PLEFT.contains(&map[*l][*c + 1]) {
-            if vec.len() > 2 && map[*l][*c + 1] == START {
-                // loop found
-                break vec;
-            }
-            if !vec.contains(&(*l, c + 1)) {
-                let mut newvec = vec.clone();
-                newvec.push((*l, *c + 1));
-                queue.push(newvec)
-            }
+        if c < w - 1 && PR.contains(ch) && PL.contains(&map[l][c + 1]) && vm[l][c + 1] == '.' {
+            queue.push((l, c + 1))
         }
-    };
-    println!("Loop: {}", loop_idxs.len());
-    // println!("Loop: {loop_idxs:?}");
+    }
 
-    println!("Part 2 result: {}", str_input.len())
+    // Count points "inside" pipes in vm by edge counting
+    let mut sum = 0;
+    let (mut counting, mut above, mut below);
+    for line in vm {
+        (counting, above, below) = (false, false, false);
+        for c in line {
+            match c {
+                '.' if counting => sum += 1,
+                '|' => counting = !counting,
+                'J' | 'L' if below => {
+                    counting = !counting;
+                    below = false
+                }
+                '7' | 'F' if above => {
+                    counting = !counting;
+                    above = false
+                }
+                'J' | 'L' => above = !above,
+                '7' | 'F' => below = !below,
+                _ => (),
+            }
+        }
+    }
+    println!("Part 2 result: {}", sum)
 }
