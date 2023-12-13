b25bb15ebc79e6ac25ddac8326d305ba65f85252
Matt Puentes <mpuentes@mitre.org>
2023-12-13 12:08:46 -0500

    Day 13

--- day_13/src/solutions.rs
+++ day_13/src/solutions.rs
@@ -1,7 +1,88 @@
 pub fn pt_1(str_input: &str) {
-    println!("Part 1 result: {}", str_input.len())
+    let maps: Vec<Vec<Vec<char>>> = str_input
+        .split("\n\n")
+        .map(|s| s.lines().map(|s| s.chars().collect()).collect())
+        .collect();
+
+    let mut sum = 0;
+    for map in maps {
+        let h = map.len();
+        let w = map[0].len();
+
+        // Try flipping horizontally
+        for i in 1..w {
+            let s = i.min(w - i);
+            if (i - s..i)
+                .zip((i..i + s).rev())
+                .all(|(ir, il)| (0..h).all(|x| map[x][ir] == map[x][il]))
+            {
+                sum += i;
+                continue;
+            }
+        }
+
+        // Try flipping vertically
+        for i in 1..h {
+            let s = i.min(h - i);
+            if (i - s..i)
+                .zip((i..i + s).rev())
+                .all(|(it, ib)| (0..w).all(|x| map[it][x] == map[ib][x]))
+            {
+                sum += i * 100;
+            }
+        }
+    }
+
+    // 33728
+    println!("Part 1 result: {}", sum)
 }
 
 pub fn pt_2(str_input: &str) {
-    println!("Part 2 result: {}", str_input.len())
+    let maps: Vec<Vec<Vec<char>>> = str_input
+        .split("\n\n")
+        .map(|s| s.lines().map(|s| s.chars().collect()).collect())
+        .collect();
+
+    let mut sum = 0;
+    for map in maps {
+        let h = map.len();
+        let w = map[0].len();
+
+        // Try flipping horizontally
+        for i in 1..w {
+            let s = i.min(w - i);
+
+            let mistakes: usize = (i - s..i)
+                .zip((i..i + s).rev())
+                .map(|(ir, il)| {
+                    (0..h)
+                        .map(|x| (map[x][ir] != map[x][il]) as usize)
+                        .sum::<usize>()
+                })
+                .sum();
+            if mistakes == 1 {
+                sum += i;
+                continue;
+            }
+        }
+
+        // Try flipping vertically
+        for i in 1..h {
+            let s = i.min(h - i);
+            let mistakes: usize = (i - s..i)
+                .zip((i..i + s).rev())
+                .map(|(it, ib)| {
+                    (0..w)
+                        .map(|x| (map[it][x] != map[ib][x]) as usize)
+                        .sum::<usize>()
+                })
+                .sum();
+            if mistakes == 1 {
+                sum += i * 100;
+            }
+        }
+    }
+
+    // 28235
+    println!("Part 2 result: {}", sum)
 }
