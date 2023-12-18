ac58d073762291c7b7b6e53d084a4bc6e1261bae
Matt Puentes <mpuentes@mitre.org>
2023-12-18 15:02:25 -0500

    Day 17

--- day_17/src/solutions.rs
+++ day_17/src/solutions.rs
@@ -1,19 +1,123 @@
-#[derive(PartialEq, Debug, Clone, Copy)]
+use pathfinding::prelude::astar;
+
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 enum Dir {
+    None,
     N(u8),
     S(u8),
     E(u8),
     W(u8),
 }
 
-impl Dir {
-    fn val(&self) -> usize {
-        match self {
-            Dir::N(_) => 0,
-            Dir::S(_) => 1,
-            Dir::E(_) => 2,
-            Dir::W(_) => 3,
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+struct Pos(usize, usize, Dir);
+
+impl Pos {
+    fn distance(&self, other: &Pos) -> usize {
+        self.0.abs_diff(other.0) + self.1.abs_diff(other.1)
+    }
+
+    fn successors_pt1(&self, map: &[Vec<u8>], h: usize, w: usize) -> Vec<(Pos, usize)> {
+        let &Pos(l, c, dir) = self;
+        let neighbors: Vec<Dir> = match &dir {
+            Dir::None => vec![Dir::N(0), Dir::S(0), Dir::E(0), Dir::W(0)],
+            Dir::N(2) => vec![Dir::E(0), Dir::W(0)],
+            Dir::S(2) => vec![Dir::E(0), Dir::W(0)],
+            Dir::E(2) => vec![Dir::N(0), Dir::S(0)],
+            Dir::W(2) => vec![Dir::N(0), Dir::S(0)],
+            Dir::N(c) => vec![Dir::N(c + 1), Dir::E(0), Dir::W(0)],
+            Dir::S(c) => vec![Dir::S(c + 1), Dir::E(0), Dir::W(0)],
+            Dir::E(c) => vec![Dir::E(c + 1), Dir::N(0), Dir::S(0)],
+            Dir::W(c) => vec![Dir::W(c + 1), Dir::N(0), Dir::S(0)],
+        };
+
+        neighbors
+            .iter()
+            .filter_map(|dir| match dir {
+                n @ Dir::N(_) if l != 0 => Some((Pos(l - 1, c, *n), map[l - 1][c] as usize)),
+                n @ Dir::S(_) if l != h - 1 => Some((Pos(l + 1, c, *n), map[l + 1][c] as usize)),
+                n @ Dir::E(_) if c != w - 1 => Some((Pos(l, c + 1, *n), map[l][c + 1] as usize)),
+                n @ Dir::W(_) if c != 0 => Some((Pos(l, c - 1, *n), map[l][c - 1] as usize)),
+                Dir::None => unreachable!(),
+                _ => None,
+            })
+            .collect()
+    }
+
+    fn successors_pt2(&self, map: &[Vec<u8>], h: usize, w: usize) -> Vec<(Pos, usize)> {
+        let &Pos(l, c, dir) = self;
+        let neighbors: Vec<Dir> = match &dir {
+            Dir::None => vec![Dir::N(3), Dir::S(3), Dir::E(3), Dir::W(3)],
+            Dir::N(c) if *c < 3 => vec![Dir::N(3)],
+            Dir::S(c) if *c < 3 => vec![Dir::S(3)],
+            Dir::E(c) if *c < 3 => vec![Dir::E(3)],
+            Dir::W(c) if *c < 3 => vec![Dir::W(3)],
+            Dir::N(9) => vec![Dir::E(3), Dir::W(3)],
+            Dir::S(9) => vec![Dir::E(3), Dir::W(3)],
+            Dir::E(9) => vec![Dir::N(3), Dir::S(3)],
+            Dir::W(9) => vec![Dir::N(3), Dir::S(3)],
+            Dir::N(c) => vec![Dir::N(c + 1), Dir::E(3), Dir::W(3)],
+            Dir::S(c) => vec![Dir::S(c + 1), Dir::E(3), Dir::W(3)],
+            Dir::E(c) => vec![Dir::E(c + 1), Dir::N(3), Dir::S(3)],
+            Dir::W(c) => vec![Dir::W(c + 1), Dir::N(3), Dir::S(3)],
+        };
+
+        neighbors
+            .iter()
+            .filter_map(|dir| match dir {
+                n @ Dir::N(x) if *x == 3 && l > 4 => Some((
+                    Pos(l - 4, c, *n),
+                    (1..=4).map(|x| map[l - x][c] as usize).sum(),
+                )),
+                n @ Dir::S(x) if *x == 3 && l < h - 4 => Some((
+                    Pos(l + 4, c, *n),
+                    (1..=4).map(|x| map[l + x][c] as usize).sum(),
+                )),
+                n @ Dir::E(x) if *x == 3 && c < w - 4 => Some((
+                    Pos(l, c + 4, *n),
+                    (1..=4).map(|x| map[l][c + x] as usize).sum(),
+                )),
+                n @ Dir::W(x) if *x == 3 && c > 4 => Some((
+                    Pos(l, c - 4, *n),
+                    (1..=4).map(|x| map[l][c - x] as usize).sum(),
+                )),
+                n @ Dir::N(x) if *x > 3 && l != 0 => {
+                    Some((Pos(l - 1, c, *n), map[l - 1][c] as usize))
+                }
+                n @ Dir::S(x) if *x > 3 && l != h - 1 => {
+                    Some((Pos(l + 1, c, *n), map[l + 1][c] as usize))
+                }
+                n @ Dir::E(x) if *x > 3 && c != w - 1 => {
+                    Some((Pos(l, c + 1, *n), map[l][c + 1] as usize))
+                }
+                n @ Dir::W(x) if *x > 3 && c != 0 => {
+                    Some((Pos(l, c - 1, *n), map[l][c - 1] as usize))
+                }
+                Dir::None => unreachable!(),
+                _ => None,
+            })
+            .collect()
+    }
+}
+
+fn print_map(path: &[Pos], h: usize, w: usize) {
+    for l in 0..h {
+        'char: for c in 0..w {
+            for Pos(pl, pc, d) in path {
+                if *pl == l && *pc == c {
+                    match d {
+                        Dir::N(_) => print!("^"),
+                        Dir::S(_) => print!("v"),
+                        Dir::E(_) => print!(">"),
+                        Dir::W(_) => print!("<"),
+                        _ => print!("."),
+                    };
+                    continue 'char;
+                }
+            }
+            print!(".")
         }
+        println!()
     }
 }
 
@@ -25,84 +129,43 @@ pub fn pt_1(str_input: &str) {
     let h = map.len();
     let w = map[0].len();
 
-    // Set of discovered nodes with path info
-    let mut open: Vec<(usize, usize, Option<Dir>)> = vec![(0, 0, None)];
-    // for each node, the node preceeding it on the cheapest path
-    let mut cheapest: Vec<Vec<Vec<Option<(usize, usize, Dir)>>>> = vec![vec![vec![None; 4]; w]; h];
-    // the cost of the cheapest path from start to point
-    let mut score: Vec<Vec<usize>> = vec![vec![10000000000000000000; w]; h];
-    score[0][0] = 0;
-    // the estimated cost of the cheapest path from start to end through this point
-    let mut full_score: Vec<Vec<usize>> = vec![vec![10000000000000000000; w]; h];
-    full_score[0][0] = map[0][0] as usize + h + w;
-
-    let goaldir = loop {
-        open.sort_by(|(l1, c1, _), (l2, c2, _)| full_score[*l2][*c2].cmp(&full_score[*l1][*c1]));
-        let Some((l, c, dir)) = open.pop() else {
-            panic!("Out of nodes but goal hasn't been found");
-        };
-        // println!("Node ({}, {}) facing {:?}", l, c, dir);
-        if l == h - 1 && c == w - 1 {
-            // we did it reddit
-            println!("We reached the goal, score of {}", score[l][c]);
-            break dir.unwrap();
-        }
-
-        // determine neighbors
-        let neighbors = match dir {
-            Some(Dir::N(2)) => vec![Dir::E(0), Dir::W(0)],
-            Some(Dir::N(c)) => vec![Dir::N(c + 1), Dir::E(0), Dir::W(0)],
-            Some(Dir::S(2)) => vec![Dir::E(0), Dir::W(0)],
-            Some(Dir::S(c)) => vec![Dir::S(c + 1), Dir::E(0), Dir::W(0)],
-            Some(Dir::E(2)) => vec![Dir::N(0), Dir::S(0)],
-            Some(Dir::E(c)) => vec![Dir::N(0), Dir::S(0), Dir::E(c + 1)],
-            Some(Dir::W(2)) => vec![Dir::N(0), Dir::S(0)],
-            Some(Dir::W(c)) => vec![Dir::N(0), Dir::S(0), Dir::W(c + 1)],
-            None => vec![Dir::N(0), Dir::S(0), Dir::E(0), Dir::W(0)],
-        };
-
-        println!("Neighbors for {l},{c},{dir:?}: {neighbors:?}");
-        for neighbor in neighbors {
-            let (nl, nc) = match neighbor {
-                Dir::N(_) if l != 0 => (l - 1, c),
-                Dir::S(_) if l != h - 1 => (l + 1, c),
-                Dir::E(_) if c != 0 => (l, c - 1),
-                Dir::W(_) if c != w - 1 => (l, c + 1),
-                _ => continue,
-            };
-
-            let tentative_score = score[l][c] + (map[nl][nc] as usize);
-            if tentative_score < score[nl][nc] {
-                cheapest[nl][nc][neighbor.val()] = Some((l, c, neighbor.clone()));
-                score[nl][nc] = tentative_score;
-                full_score[nl][nc] = tentative_score + (map[nl][nc] as usize) + (h - nl) + (w - nc);
-                let entry = (nl, nc, Some(neighbor));
-                if !open.contains(&entry) {
-                    open.push(entry)
-                }
-            }
-        }
+    let goal: Pos = Pos(h, w, Dir::None);
+    let Some((_path, cost)) = astar(
+        &Pos(0, 0, Dir::None),
+        |p| p.successors_pt1(&map, h, w),
+        |p| map[p.0][p.1] as usize + p.distance(&goal),
+        |p| p.0 == (h - 1) && p.1 == (w - 1),
+    ) else {
+        panic!("No path found!")
     };
 
-    let mut path = vec![(h - 1, w - 1, goaldir.clone())];
-    let mut cursor = (h - 1, w - 1, goaldir);
-    let mut sum = map[cursor.0][cursor.1] as usize;
-    loop {
-        let Some(cheapest) = cheapest[cursor.0][cursor.1][cursor.2.val()] else {
-            break;
-            // panic!("While reconstructing, {cursor:?} never recorded a previous node")
-        };
-        cursor = cheapest;
-        path.push(cheapest);
-        // println!("point {:?} cost {}", cursor, map[cursor.0][cursor.1]);
+    print_map(&_path, h, w);
 
-        sum += map[cursor.0][cursor.1] as usize;
-    }
-    println!("Path: {:?}", path);
-
-    println!("Part 1 result: {}", sum)
+    println!("Part 1 result: {}", cost)
 }
 
 pub fn pt_2(str_input: &str) {
-    println!("Part 2 result: {}", str_input.len())
+    let map: Vec<Vec<u8>> = str_input
+        .lines()
+        .map(|s| s.chars().map(|c| c.to_digit(10).unwrap() as u8).collect())
+        .collect();
+    let h = map.len();
+    let w = map[0].len();
+
+    let goal: Pos = Pos(h, w, Dir::None);
+    let Some((_path, cost)) = astar(
+        &Pos(0, 0, Dir::None),
+        |p| p.successors_pt2(&map, h, w),
+        |p| map[p.0][p.1] as usize + p.distance(&goal),
+        |p| p.0 == (h - 1) && p.1 == (w - 1) && match p.2 {
+            Dir::None => false,
+            Dir::N(c) | Dir::S(c) | Dir::E(c) | Dir::W(c) =>  c >= 3
+        },
+    ) else {
+        panic!("No path found!")
+    };
+    println!("{:?}", _path);
+    print_map(&_path, h, w);
+
+    println!("Part 2 result: {}", cost)
 }
