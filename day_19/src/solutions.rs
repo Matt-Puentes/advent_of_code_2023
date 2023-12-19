a0e643e3189904bb6873c36015b5c014f3185b8a
Matt Puentes <mpuentes@mitre.org>
2023-12-19 12:04:24 -0500

    Day 19 comment/cleanup

--- day_19/src/solutions.rs
+++ day_19/src/solutions.rs
@@ -1,4 +1,3 @@
-#[derive(Debug)]
 enum RuleResult<'a> {
     Rule(&'a str),
     Accept,
@@ -15,9 +14,7 @@ impl<'a> From<&'a str> for RuleResult<'a> {
     }
 }
 
-#[derive(Debug)]
 enum Rule<'a> {
-    // Index, val, result
     GT(usize, usize, RuleResult<'a>),
     LT(usize, usize, RuleResult<'a>),
     End(RuleResult<'a>),
@@ -33,70 +30,60 @@ impl<'a> Rule<'a> {
     }
 }
 
+impl<'a> Rule<'a> {
+    fn new(gt: bool, idx: &'a str, num: &'a str, result: &'a str) -> Self {
+        let i = match idx {
+            "x" => 0,
+            "m" => 1,
+            "a" => 2,
+            "s" => 3,
+            _ => panic!("Bad rule code"),
+        };
+        let n = num.parse().unwrap();
+        let r = result.into();
+        if gt {
+            Rule::GT(i, n, r)
+        } else {
+            Rule::LT(i, n, r)
+        }
+    }
+}
+
 pub fn pt_1(str_input: &str) {
-    let (rule_str, shape_strs) = str_input.split_once("\n\n").unwrap();
-    let shapes: Vec<[usize; 4]> = shape_strs
-        .lines()
-        .map(|shape| {
-            // let &[x, m, a, s]: &[usize; 4] =
-            shape[1..shape.len() - 1]
-                .split(',')
-                .map(|val| val[2..].parse().unwrap())
-                .collect::<Vec<_>>()[..]
-                .try_into()
-                .expect("4 values in a rule")
-
-            // (x, m, a, s)
-        })
-        .collect();
-
+    let (rule_str, shapes) = str_input.split_once("\n\n").unwrap();
     let rules: Vec<(&str, Vec<Rule>)> = rule_str
         .lines()
         .map(|rule| {
-            //
             let (name, vals) = rule.split_once('\u{007b}').unwrap();
-            (
-                name,
-                vals[..vals.len() - 1]
-                    .split(',')
-                    .map(|rule| {
-                        if let Some((idx, val)) = rule.split_once('>') {
-                            let (num, result) = val.split_once(':').unwrap();
-                            Rule::GT(
-                                match idx {
-                                    "x" => 0,
-                                    "m" => 1,
-                                    "a" => 2,
-                                    "s" => 3,
-                                    _ => panic!("Bad rule code"),
-                                },
-                                num.parse().unwrap(),
-                                result.into(),
-                            )
-                        } else if let Some((idx, val)) = rule.split_once('<') {
-                            let (num, result) = val.split_once(':').unwrap();
-                            Rule::LT(
-                                match idx {
-                                    "x" => 0,
-                                    "m" => 1,
-                                    "a" => 2,
-                                    "s" => 3,
-                                    _ => panic!("Bad rule code"),
-                                },
-                                num.parse().unwrap(),
-                                result.into(),
-                            )
-                        } else {
-                            Rule::End(rule.into())
-                        }
-                    })
-                    .collect(),
-            )
+            let rules = vals[..vals.len() - 1]
+                .split(',')
+                .map(|rule| {
+                    if let Some((idx, val)) = rule.split_once('>') {
+                        let (num, result) = val.split_once(':').unwrap();
+                        Rule::new(true, idx, num, result)
+                    } else if let Some((idx, val)) = rule.split_once('<') {
+                        let (num, result) = val.split_once(':').unwrap();
+                        Rule::new(false, idx, num, result)
+                    } else {
+                        Rule::End(rule.into())
+                    }
+                })
+                .collect();
+            (name, rules)
         })
         .collect();
 
     let mut sum = 0;
-    'shape: for shape in shapes {
+    'shape: for shape_str in shapes.lines() {
+        // Parse shape
+        let shape: [usize; 4] = shape_str[1..shape_str.len() - 1]
+            .split(',')
+            .map(|val| val[2..].parse().unwrap())
+            .collect::<Vec<_>>()[..]
+            .try_into()
+            .expect("4 values in a rule");
+
+        // Run shape through rules until accepted or rejected
         let mut curr_rule = rules.iter().find(|r| r.0 == "in").expect("rule named in");
         loop {
             let (_, tests) = curr_rule;
@@ -130,70 +117,43 @@ pub fn pt_1(str_input: &str) {
 
 pub fn pt_2(str_input: &str) {
     let (rule_str, _) = str_input.split_once("\n\n").unwrap();
-
     let rules: Vec<(&str, Vec<Rule>)> = rule_str
         .lines()
         .map(|rule| {
-            //
             let (name, vals) = rule.split_once('\u{007b}').unwrap();
-            (
-                name,
-                vals[..vals.len() - 1]
-                    .split(',')
-                    .map(|rule| {
-                        if let Some((idx, val)) = rule.split_once('>') {
-                            let (num, result) = val.split_once(':').unwrap();
-                            Rule::GT(
-                                match idx {
-                                    "x" => 0,
-                                    "m" => 1,
-                                    "a" => 2,
-                                    "s" => 3,
-                                    _ => panic!("Bad rule code"),
-                                },
-                                num.parse().unwrap(),
-                                result.into(),
-                            )
-                        } else if let Some((idx, val)) = rule.split_once('<') {
-                            let (num, result) = val.split_once(':').unwrap();
-                            Rule::LT(
-                                match idx {
-                                    "x" => 0,
-                                    "m" => 1,
-                                    "a" => 2,
-                                    "s" => 3,
-                                    _ => panic!("Bad rule code"),
-                                },
-                                num.parse().unwrap(),
-                                result.into(),
-                            )
-                        } else {
-                            Rule::End(rule.into())
-                        }
-                    })
-                    .collect(),
-            )
+            let rules = vals[..vals.len() - 1]
+                .split(',')
+                .map(|rule| {
+                    if let Some((idx, val)) = rule.split_once('>') {
+                        let (num, result) = val.split_once(':').unwrap();
+                        Rule::new(true, idx, num, result)
+                    } else if let Some((idx, val)) = rule.split_once('<') {
+                        let (num, result) = val.split_once(':').unwrap();
+                        Rule::new(false, idx, num, result)
+                    } else {
+                        Rule::End(rule.into())
+                    }
+                })
+                .collect();
+            (name, rules)
         })
         .collect();
 
     type Remain = [(usize, usize); 4];
 
-    let mut process_queue: Vec<(&str, Remain)> =
-        vec![("in", [(1, 4000), (1, 4000), (1, 4000), (1, 4000)])];
-
-    let mut sum: u64 = 0;
+    let mut sum: usize = 0;
+    let mut process_queue: Vec<(&str, Remain)> = vec![("in", [(1, 4000); 4])];
     loop {
-        let Some((name, mut remainders)) = process_queue.pop() else {
-            // we're done
-            break;
-        };
+        let Some((name, mut remainders)) = process_queue.pop() else { break };
+
         let (_, rules) = rules.iter().find(|r| r.0 == name).expect("rule");
+
         for rule in rules {
             let (filtered, rr) = match rule {
                 rule @ (Rule::LT(i, v, r) | Rule::GT(i, v, r)) => {
-                    // check for overlap
                     let mut filtered = remainders;
                     let (l, h) = remainders[*i];
+                    // check for overlap
                     match (rule.check(l), rule.check(h)) {
                         (true, true) => {
                             // Whole range is applicable
@@ -218,20 +178,26 @@ pub fn pt_2(str_input: &str) {
                     };
                     (filtered, r)
                 }
+                // always fully applicable, always the last rule so we don't care about editing the remainders
                 Rule::End(r) => (remainders, r),
             };
 
             match rr {
+                // provide new ranges to child rule
                 RuleResult::Rule(rn) => process_queue.push((rn, filtered)),
+                // Compute the number of shapes in the remaining ranges, accept them all
                 RuleResult::Accept => {
                     sum += filtered
                         .iter()
-                        .map(|(l, h)| ((h - l) + 1) as u64)
-                        .product::<u64>()
+                        .map(|(l, h)| ((h - l) + 1))
+                        .product::<usize>()
                 }
+                // Ignore these shapes
                 RuleResult::Reject => (),
             }
 
+            // Check if any of the remainders are of size 0- if so, it can't possibly add to the # of accepted shapes,
+            //  so we can break early.
             if remainders.iter().any(|(l, h)| l == h) {
                 break;
             }
