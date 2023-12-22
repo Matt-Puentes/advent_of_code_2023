f50e0cfa9298b8d2fbe6bc2d7f0f02b15aa7e04c
Matt Puentes <mpuentes@mitre.org>
2023-12-22 13:50:32 -0500

    Improve setup script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
-#[ -d "day_$1" ] && echo "Directory day_$1 exists." && exit
+setup_day () {
+	[ -d "day_$1" ] && echo "Directory day_$1 exists." && return
+	cp -r day_0 day_$1
+	sed -i '' "s/day_0/day_$1/g" day_$1/Cargo.toml
+	sed -i '' "s/day 0/day $1/g" day_$1/src/main.rs	
+}
 
-#cp -r day_0 day_$1
-#sed -i '' "s/day_0/day_$1/g" day_$1/Cargo.toml
-#sed -i '' "s/day 0/day $1/g" day_$1/src/main.rs
-#code day_$1
-
-sed -i '' '1s/^/use shared::Solution;\'$'\n/g' day_$1/src/solutions.rs
-sed -i '' s/"pub fn pt_1(str_input: \&str) {"/"pub fn pt_1(str_input: \&str) -> Solution {"/g day_$1/src/solutions.rs
-# sed -i '' s/"pub fn pt_2(str_input: \&str) {"/"pub fn pt_2(str_input: \&str) -> Solution {"/g day_$1/src/solutions.rs
+for i in $(seq 1 9); do setup_day 0$i; done
+for i in $(seq 10 25); do setup_day $i; done
